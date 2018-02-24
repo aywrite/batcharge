74abd735d7d2320943105d5b52742e82d09a3969
awright <awright@freelancer.com>
2018-02-24 17:26:01 +1100

    inline colors, ignore stdout result

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+// Port of the python batcharge script to rust
 use std::process::Command;
 use std::io::{self, Write};
 
@@ -35,14 +36,11 @@ fn main() {
     let filled = charge_threshold * (total_slots / 10);
     let empty = total_slots - filled;
 
-    let color_green = "[32m";
-    let color_yellow = "[33m";
-    let color_red = "[31m";
-    let color_reset = "[00m";
+    let color_reset = "%{[00m%}";
     let color = match filled {
-        0...3 => color_red,
-        4...6 => color_yellow,
-        _ => color_green,
+        0...3 => "%{[31m%}", // red
+        4...6 => "%{[33m%}", // yellow
+        _ => "%{[32m%}",     // green
     };
 
     let out = format!(
@@ -52,5 +50,5 @@ fn main() {
         "◻".repeat(empty),
         color_reset
     );
-    io::stdout().write(out.as_bytes());
+    io::stdout().write(out.as_bytes()).ok();
 }
